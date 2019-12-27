63df4e13804c12622cdaf976bebb1a0887d923fe
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-27 06:44:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -38,5 +38,5 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   assembled_aoh[1][:name] = new_value
-  assembled_aoh[1][:name]
+  assembled_aoh
 end
